21eecf6dc7aab6c7650c652eb7d4b6627d1001bf
mberkayakardev <mberkayakar@outlook.com>
2025-04-19 11:32:52 +0300

    finish

--- HeaderPublisher/Program.cs
+++ HeaderPublisher/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace DirectPublisher
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SendMessage();
+        }
+
+        static async Task SendMessage()
+        {
+            while (true)
+            {
+                #region Ekrandan okuma muhabbetleri 
+                Console.WriteLine("Lütfen bir mesaj oluşturmak için enter a bas\n\n ");
+                var message = Console.ReadLine();
+                #endregion
+
+                var factory = new ConnectionFactory();
+                factory.Uri = new Uri("amqps://lluykbjx:58YumjfZikq5pEOuqhXKxvMugdAm-nS6@collie.lmq.cloudamqp.com/lluykbjx");
+                using var connection = factory.CreateConnection();
+
+                var channel = connection.CreateModel();
+
+                /// Exchange tüpünmizi bu aşamadan sonra header exchange seçelim
+                channel.ExchangeDeclare(exchange: "HeaderExchange", type: ExchangeType.Headers, durable: true);
+
+                /// Mesajı göndermeden önce header ımızı belirliyoruz.
+                /// key string, value object olarak belirledik. 
+                /// object olduğu için herşeyi gömebilirsin, image pdf, nesne .ççç
+                Dictionary<string, object> headers = new Dictionary<string, object>();
+                headers.Add("format", "pdf");
+                headers.Add("shape", "a4");
+
+
+                /// Header bilgilerini yollamak için basic property oluşyuruyoruz. 
+                var properties = channel.CreateBasicProperties();
+                properties.Headers = headers;
+
+
+                /// Bu aşamada mesajımızı publish edeceğiz. 
+                /// Root vermiyoruz header ile göndereceğiz o yüzden root boş 
+                /// header i basic property ile göndereceğiz. 
+                channel.BasicPublish(exchange: "HeaderExchange", routingKey: string.Empty, basicProperties: properties, body: Encoding.UTF8.GetBytes(message));
+
+
+
+                Console.WriteLine(" Mesaj Gönderildi. \n\n ");
+                Console.WriteLine(" ------------------------------------------------------------------------ ");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
--- HeaderSubsriber/Program.cs
+++ HeaderSubsriber/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://lluykbjx:58YumjfZikq5pEOuqhXKxvMugdAm-nS6@collie.lmq.cloudamqp.com/lluykbjx");
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+var subscriber = new EventingBasicConsumer(channel);
+
+var queuename = channel.QueueDeclare().QueueName;
+
+/// burada da dinleyeceğimiz header mesajına göre dicrionary oluşturuyoruz 
+Dictionary<string, object> headers = new Dictionary<string, object>();
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+headers.Add("x-match", "all");
+
+/// Header Argumanlarımızı veriyoruz. 
+channel.QueueBind( queue:  queuename, exchange: "HeaderExchange", routingKey: string.Empty, arguments:  headers);
+
+channel.BasicConsume(queue: queuename, autoAck: false, consumer: subscriber);
+subscriber.Received += Subscriber_Received; /// 
+
+void Subscriber_Received(object? sender, BasicDeliverEventArgs e)
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+
+    channel.BasicAck(e.DeliveryTag, false);
+}
+
+
+Console.ReadLine();
